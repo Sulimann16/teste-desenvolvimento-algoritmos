7fa65d9dde5ff673376ffd6c832408695d20332d
Sulimann16 <mateussulimann@gmail.com>
2026-05-19 19:07:52 -0300

    app.js

--- app.js
+++ app.js
@@ -1,46 +1,92 @@
-const readline = require('readline-sync')
+const readline = require('readline-sync');
+
+// =================================================================
+// DESAFIO: TOTEM MEGALODON
+// =================================================================
 
 function iniciarTotem() {
-    console.log("=== BEM-VINDO AO TECHPARK ===")
-    console.log("Iniciando o sistema da Montanha-Russa MegaloDon...\n")
+    console.log("=== BEM-VINDO AO TECHPARK ===");
+    console.log("Iniciando o sistema da Montanha-Russa MegaloDon...\n");
 
-    // 1 ENTRADA E VALIDACAO DO NOME
-    let nome = readline.question("Digite o nome do cliente: ")
+    // EXEMPLO DE ENTRADA:
+    // O readline.question() lê o que o usuário digita como TEXTO (String)
 
-    if (nome === "") {
-        console.log("[ERRO] O nome do cliente não pode estar vazio!")
+    // =================================================================
+    // VERIFICAÇÃO DE NOME:
+
+    let nome = readline.question("Digite o nome do cliente: ");
+    if (nome === "") //Verifica se o nome é vazio
+    {
+        console.log("[ERRO] O nome do cliente não pode estar vazio!\n");
         return
     }
 
-    // 2 ENTRADA E VALLIDACAO DE IDADE
-    let idade = readline.questionInt("Digite a idade do cliente: ")
-
-    if (idade <= 0 || idade > 120) {
-        console.log("[ERRO] Idade inválida! Digite um valor entre 1 e 120.")
+    else if (!isNaN(nome)) //Verifica se o nome é um número.
+    {
+        console.log("[ERRO] O nome do cliente não pode ser um número!\n");
         return
     }
 
-    // 3 ENTRADA E VALIDAÇÃO DA ALTURA
-    let altura = readline.questionFloat("Digite a altura do cliente (ex: 1.75): ")
+    else //Se o nome for válido, exibe a mensagem de boas-vindas.
+    {
+        console.log("\nOlá " + nome + ", Seja bem vindo a montanha-russa MegaloDon!\n");
+    }
 
-    if (altura <= 0.50 || altura > 2.50) {
-        console.log("[ERRO] Altura inválida! A altura deve estar entre 0.50m e 2.50m.")
+    // =================================================================
+    // VERIFICAÇÃO DE IDADE:
+
+    let idade = readline.questionInt(nome + ", digite sua idade: ");
+    if (idade <= 0 || idade > 120) //Verifica se a idade é um número positivo e dentro de um limite razoável.
+    {
+        console.log("[ERRO] Idade inválida! Digite um valor entre 1 e 120.\n");
         return
     }
 
-    // 4REGRAs
-    let temIdade = idade >= 12
-    let temAltura = altura >= 1.50
-
-    if (temIdade && temAltura) {
-        console.log(`Olá, ${nome}! Cadastro validado com sucesso. Seu acesso foi PERMITIDO. Divirta-se na MegaloDon!`)
-    } else if (!temIdade && temAltura) {
-        console.log(`[ACESSO NEGADO] - ${nome}, você possui a altura necessária, mas não atingiu a idade mínima de 12 anos.`)
-    } else if (temIdade && !temAltura) {
-        console.log(`[ACESSO NEGADO] - ${nome}, você possui a idade necessária, mas não atingiu a altura mínima de 1.50m.`)
-    } else {
-        console.log(`[ACESSO NEGADO] - ${nome}, o acesso foi recusado porque você não possui a idade mínima (12 anos) e nem a altura mínima (1.50m).`)
+    else //Se a idade for válida, exibe a mensagem de registro bem-sucedido.
+    {
+        console.log("\nIdade " + idade + " anos, registrada com sucesso!\n");
     }
+
+    // =================================================================
+    // VERIFICAÇÃO DA ALTURA:
+
+    let altura = readline.questionFloat("Digite a sua altura " + nome + " :");
+
+    if (altura <= 0.50 || altura > 2.50) //Verifica se a altura é permitida para a montanha-russa (entre 0.50m e 2.5m).
+    {
+        console.log("[ERRO] Altura inválida! A altura deve ser entre 0.50m e 2.5m.\n");
+        return
+    }
+
+    else //Se a altura for válida, exibe a mensagem de registro bem-sucedido. 
+    {
+        console.log("\nAltura " + altura + " metros, registrada com sucesso!\n");
+    }
+
+    // =================================================================
+    //VERIFICAÇÃO FINAL DE ACESSO:
+
+    if (idade >= 12 && altura >= 1.50) //Verifica se o cliente atende aos requisitos de idade e altura para acessar a montanha-russa.
+    {
+        console.log("Olá, " + nome + "! Cadastro validado com sucesso. Seu acesso foi PERMITIDO. \n Divirta-se na MegaloDon!\n");
+    }
+
+    else if (altura >= 1.50 && idade < 12) //Verifica se o cliente tem a altura necessária, mas não atingiu a idade mínima.
+    {
+        console.log("[ACESSO NEGADO]" + nome + ", você possui a altura necessária, mas não atingiu a idade mínima de 12 anos.\n")
+    }
+
+    else if (idade >= 12 && altura < 1.50) //Verifica se o cliente tem a idade necessária, mas não atingiu a altura mínima.
+    {
+        console.log("[ACESSO NEGADO]" + nome + ", você possui a idade necessária, mas não atingiu a altura mínima de 1.50m.\n")
+    }
+
+    else if (idade < 12 && altura < 1.50) //Verifica se o cliente não atende aos requisitos de idade e altura para acessar a montanha-russa.
+    {
+        console.log("[ACESSO NEGADO]" + nome + ", o acesso foi recusado porque você não possui a idade mínima (12 anos) e nem a altura mínima (1.50m).\n")
+    }
+
 }
 
-iniciarTotem()
+//Iniciando o sistema do Totem MegaloDon
+iniciarTotem();
